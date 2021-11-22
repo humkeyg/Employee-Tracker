2cf5365bd33cd9eb49d27e7af7975fc75605a60a
humkeyg <87458845+humkeyg@users.noreply.github.com>
2021-11-21 22:00:54 -0800

    working on mysql workbench connection

--- app.js
+++ app.js
@@ -10,14 +10,16 @@ const db = mysql.createConnection(
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
   },
-  console.log(`Connected to the company_db database.`)
 );
 
-// db.connect(function (err) {
-//   if (err) throw err;
-//   console.log("connected as id " + connection.threadId + "\n");
-//   promptUser();
-// });
+// module.exports = db;
+
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + db.threadId + "\n");
+  console.log(`Connected to the company_db database.`)
+  promptUser();
+});
 
 function promptUser() {
   inquirer.prompt([{
@@ -173,4 +175,4 @@ function updateEmployeeRole() {
 
 }
 
-promptUser()
+// promptUser()
--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS company_db;
-Create database company_db;
-use company_db;
-Create table departments(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(30));
-Create table managers(id INT PRIMARY KEY AUTO_INCREMENT, first_name VARCHAR (30), last_name VARCHAR (30));
-Create table roles(id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(30), salary DECIMAL, department_id INT);
-Create table employees(id INT PRIMARY KEY AUTO_INCREMENT, first_name VARCHAR (30), last_name VARCHAR(30), role_id INT, manager_id INT, department_id INT);
+CREATE DATABASE company_db;
+USE company_db;
+CREATE TABLE departments(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(30));
+CREATE TABLE managers(id INT PRIMARY KEY AUTO_INCREMENT, first_name VARCHAR (30), last_name VARCHAR (30));
+CREATE TABLE roles(id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(30), salary DECIMAL, department_id INT);
+CREATE TABLE employees(id INT PRIMARY KEY AUTO_INCREMENT, first_name VARCHAR (30), last_name VARCHAR(30), role_id INT, manager_id INT, department_id INT);
